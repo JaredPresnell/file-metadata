efaed7ad67acf2427732aea42cef0188315fb4d4
System <none>
2017-07-24 18:46:47 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -4,12 +4,19 @@
 var express = require('express');
 var multer = require('multer');
 var app = express();
+var uploadService = multer({ storage: multer.memoryStorage() });
+
 
 app.use(express.static('public'));
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
+
+app.get("/upload*", uploadService.single('file'), function(req, res){
+  var uploadedFile = req.file;
+  var fileSize = uploadedFile.size
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
